ceeed746e5ea4102d13aab2275edbaebd3b23c86
shinjons99 <shinjons99@gmail.com>
2023-02-21 20:47:26 +0600

    after reduce and dot notation

---
+++ reduce.js
@@ -0,0 +1,12 @@
+//*map , foreach, filter, find, reduce
+
+
+const numbers = [1, 2, 3, 4, 5];
+//* .reduce (accumulatorFunction, initial value)
+//* accumulatorFunction use two parameters 
+//* const total = numbers.reduce((previous, current)=> previous + current, 0);
+const total = numbers.reduce((previous, current)=>{
+    console.log(previous, current);
+    return previous + current
+}, 0);
+
