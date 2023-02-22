8d4f24674adbeedf6d02f221d32d5214133cc79f
shinjons99 <shinjons99@gmail.com>
2023-02-22 14:48:58 +0600

    after quiz

---
+++ summary.js
@@ -0,0 +1,19 @@
+//* 1. optional chaining 
+
+//* student.marks?.math
+
+//* 2. map, filter, find
+
+//* 3. (optional): forEach, reduce(advance)
+
+//* 4. class
+
+//* 5. (optional) inheritance , super
+
+//* 6. (must) prototypical  Inheritance
+
+
+const numbers = [1, 3, 5, 7, 8, 9];
+
+const evenNumber = numbers.map(number => number % 2 === 0);
+console.log(evenNumber);
